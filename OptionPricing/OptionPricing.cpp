89d0fbb29c925d2fe1f6124157e7f4bf6c16ec53
Paul Grigoras <paul.grigoras90@gmail.com>
2014-06-21 12:55:38 +0100

    Remove extra unload group call from option pricing dfe impl.

--- OptionPricing/OptionPricing.cpp
+++ OptionPricing/OptionPricing.cpp
@@ -152,7 +152,6 @@ double optionPricing(double strike, double sigma, double timestep, int numMaturi
   for (int i = 0; i < numEngines; i++)
     free(actions[i]);
 
-  max_unload_group(group);
   max_file_free(maxfile);
 
   real sum = 0;
